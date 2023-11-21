9a5f612ae20128ce4f992fe65992956056ddcaa5
keyvan <146589626+keyvan1214@users.noreply.github.com>
2023-11-21 12:14:10 +0330

    web 16.php

---
+++ session2/uuu.php
@@ -0,0 +1,39 @@
+<?php
+
+$firstname = "Morteza "; 
+$lastname = "Ghoddousi";
+$fullname = $firstname.$lastname;
+echo $firstname;
+$age = 29;
+$score = 18.94; 
+$check = true; 
+echo "<br>";
+echo $age;
+echo "<br>";
+echo $score;
+echo "<br>";
+echo $check;
+echo "<br>";
+echo $fullname;
+echo "<br>";
+$x = 5;
+$y = 4;
+echo $x+$y;
+echo "<br>";
+
+echo $x-$y;
+echo "<br>";
+
+echo $x*$y;
+echo "<br>";
+
+echo $x/$y;
+echo "<br>";
+
+echo $x%$y;
+echo "<br>";
+
+
+
+?>
+
